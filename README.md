99dcfc5a0e90a9deec1ff184d278eb94ebec46ea
xuyan <3426373@qq.com>
2024-05-30 04:45:53 +0800

    init commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Fatcat ERP
-基于最新的springcloud架构
+基于最新的springcloud，gradle构建，JAVA21，为未来打造的ERP微服务体系，也是对新技术的一种尝试学习。
 
 ## 主要技术栈
 * springboot 3.2.6
--- services/system/src/main/java/com/fatcat/erp/services/system/SystemServiceApplication.java
+++ services/system/src/main/java/com/fatcat/erp/services/system/SystemServiceApplication.java
@@ -1,13 +1,14 @@
 package com.fatcat.erp.services.system;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SystemServiceApplication {
     public static void main(String[] args){
-        SpringApplication.run(SystemServiceApplication.class, args);
+        new SpringApplicationBuilder(SystemServiceApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
